ec8b1f4cf0fd3becb2e71153fdc18b2d2af855b0
Marcin Bielak <marcin.bieli@gmail.com>
2026-07-26 13:32:05 +0200

    add KafkaIO object impl.

---
+++ src/main/scala/com/streamspecs/kafka/KafkaIO.scala
@@ -0,0 +1,44 @@
+package com.streamspecs.kafka
+
+import cats.effect.{IO, Resource}
+import com.streamspecs.config.KafkaConfig
+import com.streamspecs.pipeline.ValidationPipeline.RoutedEvent
+import fs2.Stream
+import fs2.kafka.*
+
+object KafkaIO:
+
+  def consumerSettings(cfg: KafkaConfig): ConsumerSettings[IO, String, String] =
+    ConsumerSettings[IO, String, String]
+      .withBootstrapServers(cfg.bootstrapServers)
+      .withGroupId(cfg.groupId)
+      .withClientId(cfg.clientId)
+      .withAutoOffsetReset(
+        if cfg.autoOffsetReset.equalsIgnoreCase("earliest") then AutoOffsetReset.Earliest
+        else AutoOffsetReset.Latest
+      )
+      .withEnableAutoCommit(false)
+
+  def producerSettings(cfg: KafkaConfig): ProducerSettings[IO, String, String] =
+    ProducerSettings[IO, String, String]
+      .withBootstrapServers(cfg.bootstrapServers)
+      .withClientId(s"${cfg.clientId}-producer")
+
+  def consume(cfg: KafkaConfig): Stream[IO, CommittableConsumerRecord[IO, String, String]] =
+    KafkaConsumer
+      .stream(consumerSettings(cfg))
+      .subscribeTo(cfg.topics.incoming)
+      .records
+
+  def produceAndCommit(
+      producer: KafkaProducer[IO, String, String],
+      key: Option[String],
+      routed: RoutedEvent,
+      offset: CommittableOffset[IO]
+  ): IO[Unit] =
+    val record = ProducerRecord(routed.targetTopic, key.getOrElse(""), routed.payload)
+    producer.produce(ProducerRecords.one(record)).flatten.void *> offset.commit
+
+  def producerResource(cfg: KafkaConfig): Resource[IO, KafkaProducer[IO, String, String]] =
+    KafkaProducer.resource(producerSettings(cfg))
+end KafkaIO
